89aa4e3bc6c403e5a8752ece70c68f9d74961633
deepakdukare <deepakdukare2020@gmail.com>
2026-04-08 22:26:35 +0530

    Update swagger.js

--- backend/src/swagger.js
+++ backend/src/swagger.js
@@ -69,8 +69,8 @@ const spec = {
         `,
     },
     servers: [
-        //{ url: 'https://api-dr-indu-child-care.brahmaastra.ai/', description: 'Production Server' },
-        { url: 'http://localhost:5000/', description: 'Local Development' },
+        { url: 'https://api-dr-indu-child-care.brahmaastra.ai/', description: 'Production Server' },
+        //{ url: 'http://localhost:5000/', description: 'Local Development' },
     ],
     components: {},
     security: [],
@@ -1126,7 +1126,7 @@ module.exports = (app) => {
             filter: true,
         }
     }));
-    //console.log('📚 Swagger docs (Prod)  → https://api-dr-indu-child-care.brahmaastra.ai/api-docs');
-    console.log('📚 Swagger docs (Local) → http://localhost:5000/api-docs');
+    console.log('📚 Swagger docs (Prod)  → https://api-dr-indu-child-care.brahmaastra.ai/api-docs');
+    //console.log('📚 Swagger docs (Local) → http://localhost:5000/api-docs');
 };
 
